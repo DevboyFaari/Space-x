a2fd038bf0ff7bf849ff6c3f809104c52730ea39
unknown <faarijoye@gmail.com>
2022-04-04 08:14:58 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import * as ReactBootStrap from "react-bootstrap";
+
+const LAUNCHES_QUERY = `query launches {
+  launches {
+    mission_name
+    mission_id
+    rocket {
+      rocket_name
+      rocket {
+        company
+        name
+        mass {
+          kg
+        }
+      }
+    }
+    launch_site {
+      site_name
+    }
+    launch_date_local
+  }
+}
+`;
 
 function App() {
+  const [launches, setLaunches] = React.useState([]);
+  React.useEffect(() => {
+    fetch("https://api.spacex.land/graphql/", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ query: LAUNCHES_QUERY }),
+    })
+      .then((response) => response.json())
+      .then((data) => setLaunches(data.data.launches));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactBootStrap.Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>mission_name</th>
+            <th>rocket</th>
+            <th>company</th>
+            <th>name</th>
+            <th>kg</th>
+            <th>launch_date_local</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {launches.map((launch) => (
+            <tr key={launch.id}>
+              <td>{launch.mission_name}</td>
+              <td>{launch.rocket.rocket_name}</td>
+              <td>{launch.rocket.rocket.company}</td>
+              <td>{launch.rocket.rocket.name}</td>
+              <td>{launch.rocket.rocket.mass.kg}</td>
+              <td>{launch.launch_date_local}</td>
+            </tr>
+          ))}
+        </tbody>
+      </ReactBootStrap.Table>
     </div>
   );
 }
